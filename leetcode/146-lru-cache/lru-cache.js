9f8212457682e4bd3cf3b5b5a962989012a73df2
Pratham Jha <prathamjha5683@gmail.com>
2026-08-18 23:37:57 +0530

    Time: 21 ms (99.86%) | Memory: 72.2 MB (98.93%) - LeetSync

---
+++ leetcode/146-lru-cache/lru-cache.js
@@ -0,0 +1,77 @@
+
+/** 
+ * @param {number} key
+ * @param {number} value
+*/
+class Node{
+    constructor(key,value){
+        this.key = key
+        this.value = value
+        this.prev = null
+        this.next = null
+    }    
+}
+
+
+
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function(capacity) {
+    this.capacity = capacity
+    this.map = new Map()
+    this.head = new Node(0,0)
+    this.tail = new Node(0,0)
+    this.head.next = this.tail 
+    this.tail.prev = this.head
+};
+
+LRUCache.prototype.add = function(node){
+    node.next = this.head.next
+    node.prev = this.head
+    this.head.next.prev = node
+    this.head.next = node
+
+}
+
+LRUCache.prototype.remove= function(node){
+    node.prev.next = node.next
+    node.next.prev = node.prev
+}
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {
+    if(!this.map.has(key)) return -1;
+    const node = this.map.get(key)
+    this.remove(node)
+    this.add(node)
+    return node.value
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+    if(this.map.has(key)){
+        this.remove(this.map.get(key));
+    }
+    const newNode = new Node(key,value);
+    this.add(newNode)
+    this.map.set(key,newNode);
+    if(this.map.size > this.capacity){
+        const lru = this.tail.prev
+        this.remove(lru);
+        this.map.delete(lru.key)
+    }
+};
+
+/** 
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
